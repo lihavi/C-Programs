43c1a86f6883c0b1cf2799e7f3b02ac701927efc
vivianne <viviannjoroge91@gmail.com>
2023-11-22 10:03:14 +0300

    add c programs

--- sample3.c
+++ sample3.c
@@ -3,7 +3,7 @@ int main(){
     int r,c;
     for(c=4; c<=1; c--)
     {
-        for(r=1; r<=c; r--)
+        for(r=4; r<=c; r--)
         {
             printf("%d\t ",r);
         }
---
+++ sample6.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+int a[4] = {20,35,50,80};
+int i;
+printf("Elements in a are:\n");
+for(i=0; i<=3; i++){
+    printf("%d\t", a[i]);
+}
+
+}
